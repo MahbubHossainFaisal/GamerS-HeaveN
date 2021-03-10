9fb8714207b5e2a4a86c8be15d5cda70df51c242
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-03-11 02:59:29 +0600

    installing react-bootstrap , fontawesome and using it in the header and footer

--- gamers-heaven/src/App.js
+++ gamers-heaven/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
 import Head from './Components/Header'
 import Foot from './Components/Footer'
 
@@ -6,7 +7,11 @@ const App=()=> {
   return (
     <div>
       <Head />
-      <h1>Welcome to GamerS HeaveN</h1>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to GamerS HeaveN</h1>
+        </Container>
+      </main>
       <Foot />
     </div>
   );
