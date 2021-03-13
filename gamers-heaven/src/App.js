018b6135b55c1c1e31c64eaadeddadb99fc268b7
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-03-14 01:55:14 +0600

    Implementing React router to the links

--- gamers-heaven/src/App.js
+++ gamers-heaven/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import {BrowserRouter as Router , Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Head from './Components/Header'
 import Foot from './Components/Footer'
 import HomeScreen from './screen/HomeScreen'
+import ProductScreen from './screen/ProductScreen'
 
 const App=()=> {
   return (
-    <div>
+    <Router>
       <Head />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Foot />
-    </div>
+    </Router>
   );
 }
 
