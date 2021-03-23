449121f303abcdf6096b3922ea94b90c592c31cf
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-03-24 01:46:32 +0600

    Installing nodemon,concurrency and adding them together to run the backend and frontend with one command

--- gamers-heaven/src/screen/ProductScreen.js
+++ gamers-heaven/src/screen/ProductScreen.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
 import Rating from '../Components/Rating'
 import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-    
-    const [product,setProduct] = useState({})
+
+    const [product, setProduct] = useState({})
 
     useEffect(() => {
         const fetchProduct = async () => {
@@ -15,7 +15,7 @@ const ProductScreen = ({ match }) => {
             setProduct(data)
         }
         fetchProduct()
-    }, [])
+    }, [match])
 
     return <>
         <Link className='btn btn-dark my-3' to='/'>
