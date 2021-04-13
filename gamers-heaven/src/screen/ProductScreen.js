19e0724d879bd1a7218dead327e89b3785708dcd
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-04-14 01:13:40 +0600

    Creating message for error and loader components,then productDetails reducer and product action

--- gamers-heaven/src/screen/ProductScreen.js
+++ gamers-heaven/src/screen/ProductScreen.js
@@ -1,80 +1,83 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom';
+import React, {  useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
 import Rating from '../Components/Rating'
-import axios from 'axios'
+import {listProductDetails} from '../actions/productActions'
+import Loader from '../Components/Loader'
+import Message from '../Components/Message'
+
 
 const ProductScreen = ({ match }) => {
 
-    const [product, setProduct] = useState({})
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${match.params.id}`)
-
-            setProduct(data)
-        }
-        fetchProduct()
-    }, [match])
-
+       dispatch(listProductDetails(match.params.id))
+    }, [dispatch,match])
+    const productDetails = useSelector(state => state.productDetails)
+    const {loading,error,product} = productDetails
     return <>
         <Link className='btn btn-dark my-3' to='/'>
             Go Back
         </Link>
-        <Row>
-            <Col md={6}>
-                <Image src={product.image} alt={product.name} fluid />
-                {/* fluid will make the image to be inside the container and stop it from going outside of it. */}
-            </Col>
-            <Col md={3}>
-                <ListGroup variant='flush'>
-                    {/*flush variant takes away the border */}
-                    <ListGroupItem>
-                        <h3>{product.name}</h3>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Price: ${product.price}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Description: {product.description}
-                    </ListGroupItem>
-                </ListGroup>
-            </Col>
-            <Col md={3}>
-                <Card>
-                    <ListGroupItem variant='flush'>
-                        {/*flush variant takes away the spacing */}
+        {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                    {/* fluid will make the image to be inside the container and stop it from going outside of it. */}
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        {/*flush variant takes away the border */}
                         <ListGroupItem>
-                            <Row>
-                                <Col>
-                                    Price:
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Price: ${product.price}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Description: {product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroupItem variant='flush'>
+                            {/*flush variant takes away the spacing */}
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Price:
                             </Col>
-                                <Col>
-                                    <strong>${product.price}</strong>
-                                </Col>
-                            </Row>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <Row>
-                                <Col>
-                                    Status:
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Status:
                             </Col>
-                                <Col>
-                                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                                </Col>
-                            </Row>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                            <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add To Cart</Button>
-                        </ListGroupItem>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add To Cart</Button>
+                            </ListGroupItem>
 
-                    </ListGroupItem>
-                </Card>
-            </Col>
-        </Row>
+                        </ListGroupItem>
+                    </Card>
+                </Col>
+            </Row>
+        ) }
+        
     </>
 }
 
--- gamers-heaven/src/store.js
+++ gamers-heaven/src/store.js
@@ -1,11 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import {productListReducer} from './Components/reducers/productReducers'
+import {productDetailsReducer, productListReducer} from './Components/reducers/productReducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 })
 
 const initiaState = {}
