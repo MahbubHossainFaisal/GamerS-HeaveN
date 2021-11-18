9c86653ea4eba18fec0efadb2813ae147b09d294
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-11-18 19:17:17 +0600

    Cart Screen

--- gamers-heaven/src/screen/CartScreen.js
+++ gamers-heaven/src/screen/CartScreen.js
@@ -1,9 +1,9 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { Button, Card, Col, Form, Image, ListGroup, ListGroupItem, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {Row,Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
-import Messages from '../Components/Message'
 import { addToCart } from '../Components/actions/cartActions'
+import Message from '../Components/Message'
 
 
 const CartScreen = ({match, location, history}) => {
@@ -21,10 +21,67 @@ const CartScreen = ({match, location, history}) => {
             dispatch(addToCart(productId,qty))
         }
     },[dispatch,productId,qty])
+
+    const removeFromCartHandler = (id) =>{
+        console.log('remove')
+    }
+    const checkoutHandler = () =>{
+        history.push('/login?redirect=shipping')
+    }
     return (
-        <div>
-            Cart
-        </div>
+        <Row>
+            <Col md={8}>
+            <h1>Shopping Cart</h1>
+            {cartItems.length === 0 ? <Message> Your cart is empty! <Link to='/'>Go Back</Link> </Message> : (
+                <ListGroup variant='flush'>
+                    {cartItems.map(item =>(
+                        <ListGroupItem key={item.product}>
+                        <Row className='bg-light py-2'>
+                            <Col md={2}>
+                                <Image src={item.image} alt={item.name} fluid rounded />
+                            </Col>
+                            <Col md={3}>
+                                <Link to={`/products/${item.product}`}>{item.name}</Link>
+                            </Col>
+                            <Col md={2}>
+                                {item.price}৳
+                            </Col>
+                            <Col md={2}>
+                                <Form as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product,Number(e.target.value)))}>
+                                                {[...Array(item.countInStock).keys()].map(x => (
+                                                    <option key={x + 1} value={x + 1}> {x + 1} </option>
+
+                                                ))}
+                                </Form>
+                            </Col>
+                            <Col md={2}>
+                                <Button type='button' variant='light' onClick={()=> removeFromCartHandler(item.id)}>
+                                    <i className='fas fa-trash'></i>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </ListGroupItem>
+                    ))}
+                    
+                </ListGroup>
+                
+            ) }
+            </Col>
+            <Col md={4}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                         <h2> Subtotal ({cartItems.reduce((acc,item) => acc+item.qty, 0)}) items </h2>
+                         <h3>{cartItems.reduce((acc,item) => acc+item.qty * item.price, 0).toFixed(2)}৳</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Button type='button' className='btn-block' disabled={cartItems.length===0}
+                            onClick={checkoutHandler}>Proceed to Checkout</Button>
+                        </ListGroupItem>
+                        </ListGroup>
+                </Card>
+             </Col>
+        </Row>
     )
 }
 
