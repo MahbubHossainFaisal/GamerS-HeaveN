217d4af36e993e7ad5eca4e428c731e721f79cc8
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-11-21 01:35:55 +0600

    User routes,controllers and email,pass authentication

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,29 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js'
+
+//@description : auth user and get the token
+//@route: GET /api/users/login
+//@acess: Public route
+
+const authUser = asyncHandler(async(req,res) =>{
+    const {email,password} = req.body;
+    
+    //authenticate user
+    const user = await User.findOne({email})
+
+    if(user && (await user.matchPasswords(password))){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        })
+    }
+    else{
+        res.status(401) //unAuthorized
+        throw new Error('Invalid user or password')
+    }
+})
+
+export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 
@@ -16,13 +17,14 @@ const app = express()
 //     console.log(req.originalUrl)
 //     next()
 // })
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Api is running')
 })
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users',userRoutes)
 
 app.use(notFound)
 
